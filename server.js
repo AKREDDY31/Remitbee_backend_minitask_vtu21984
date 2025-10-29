89b988722cffc91f4130be0b580d2481b06e2eb2
ANIL KUMAR REDDY CHIPATI <vtu21984@veltech.edu.in>
2025-10-29 22:33:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// server.js
-// Main entry point for the RemitBee Backend Developer Task
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
@@ -8,22 +5,15 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 const PORT = 5000;
-
-// Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-// Mount user routes
 app.use('/users', userRoutes);
 
-// Handle 404 - route not found
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
-
-// Error-handling middleware (for catching all internal errors)
 app.use(errorHandler);
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
 });
