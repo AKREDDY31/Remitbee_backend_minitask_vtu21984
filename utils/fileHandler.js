76e37c13c7a0d06bd5fed6fa5cce112607ec86c6
ANIL KUMAR REDDY CHIPATI <vtu21984@veltech.edu.in>
2025-10-29 22:32:16 +0530

    Update fileHandler.js

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,12 +1,7 @@
-// utils/fileHandler.js
-// Utility functions for reading and writing JSON file
-
 const fs = require('fs');
 const path = require('path');
 
 const filePath = path.join(__dirname, '../data/users.json');
-
-// Read users.json safely
 const readData = () => {
   try {
     const data = fs.readFileSync(filePath, 'utf8');
@@ -16,9 +11,7 @@ const readData = () => {
   }
 };
 
-// Write updated data to users.json
 const writeData = (data) => {
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 };
-
 module.exports = { readData, writeData };
